4ab06b3ddbcb33f5d45d44ff59cc74221320d64e
Dariusz Paluch <dariusz.paluch@hotmail.com>
2017-05-27 12:40:54 +0200

    Prepare first task

---
+++ src/java/com/dariuszpaluch/ant/tasks/MetalinkTask.java
@@ -0,0 +1,35 @@
+package org.ant.tasks;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+import javax.xml.bind.DatatypeConverter;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.DirectoryScanner;
+import org.apache.tools.ant.Task;
+import org.apache.tools.ant.types.FileSet;
+import org.w3c.dom.DOMException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+public class MetalinkTask extends Task {
+
+	public void execute() throws BuildException {
+		System.out.println("DARIUSZ PALUCH122");
+	}
+}
